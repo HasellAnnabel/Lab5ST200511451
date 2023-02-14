0a31595e02f3aae3ae9240effc2ee7a1d56b1e77
Hasell Jaramillo <200511451@student.georgianc.on.ca>
2023-02-13 20:34:08 -0500

    final commit

--- src/main/java/com/example/lab5st200511451/MobilePhone.java
+++ src/main/java/com/example/lab5st200511451/MobilePhone.java
@@ -1,4 +1,27 @@
 package com.example.lab5st200511451;
 
+import javafx.scene.image.Image;
+
 public class MobilePhone {
+
+    private String model;
+
+    private String path;
+
+    public MobilePhone(String model, String path){
+
+        this.model = model;
+
+        this.path = path;
+    }
+
+    public Image getImage(){
+
+        return new Image(MobilePhone.class.getResourceAsStream(path));
+    }
+
+    public String getModel(){
+
+        return this.model;
+    }
 }
--- src/main/java/com/example/lab5st200511451/MobilePhoneContoller.java
+++ src/main/java/com/example/lab5st200511451/MobilePhoneContoller.java
@@ -1,4 +1,23 @@
 package com.example.lab5st200511451;
 
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
+
 public class MobilePhoneContoller {
+
+    @FXML
+    private Text modelphone;
+
+    @FXML
+    private ImageView phoneimage;
+
+    @FXML
+    void initialize() {
+
+        MobilePhone iphone11 = new MobilePhone("Iphone11PRO", "/com/example/lab5st200511451/images/Iphone11Pro.jpeg");
+        modelphone.setText(iphone11.getModel());
+        phoneimage.setImage(iphone11.getImage());
+    }
+
 }
