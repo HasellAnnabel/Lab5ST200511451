eaf08dbce266b19470249e0cde76d93643c2a38d
Hasell Jaramillo <200511451@student.georgianc.on.ca>
2023-02-15 16:13:27 -0500

    found some errors and fixe all of them. Final commit before submission

--- src/main/java/com/example/lab5st200511451/MobilePhoneContoller.java
+++ src/main/java/com/example/lab5st200511451/MobilePhoneContoller.java
@@ -1,10 +1,14 @@
 package com.example.lab5st200511451;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
 
-public class MobilePhoneContoller {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MobilePhoneContoller implements Initializable {
 
     @FXML
     private Text modelphone;
@@ -12,12 +16,15 @@ public class MobilePhoneContoller {
     @FXML
     private ImageView phoneimage;
 
-    @FXML
-    void initialize() {
 
+
+    @Override
+
+    public void initialize (URL url, ResourceBundle resourceBundle){
         MobilePhone iphone11 = new MobilePhone("Iphone11PRO", "/com/example/lab5st200511451/images/Iphone11Pro.jpeg");
         modelphone.setText(iphone11.getModel());
         phoneimage.setImage(iphone11.getImage());
+
     }
 
 }
